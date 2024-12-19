93673060bf560be96b40d1457706a12953ac3ea8
RushieWashie <57391931+JiroCab@users.noreply.github.com>
2024-12-19 15:21:55 +0800

    pain
    - added config to limit payload commands to block interaction rate
    - added a payload enter button (buggy)
    - added settings for turning off/on modifiers

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # PayloadCommanding
 Allows you to take/drop units in command mode in v7!
-
+and now enter blocks w/o possession! (very buggy bc v7)
 # Host need to have the mod to work in multiplayer!
+
+u can build the regular way or with [Toxopid](https://github.com/Xpdustry/Toxopid) incase you need to btw 
--- src/payloadunitcommand/PUCMain.java
+++ src/payloadunitcommand/PUCMain.java
@@ -1,18 +1,19 @@
 package payloadunitcommand;
 
-import arc.Events;
+import arc.*;
 import arc.scene.Element;
 import arc.scene.event.Touchable;
 import arc.scene.ui.layout.Scl;
 import arc.scene.ui.layout.Table;
 import arc.struct.Seq;
-import arc.util.Strings;
-import arc.util.Time;
+import arc.util.*;
 import mindustry.Vars;
+import mindustry.core.*;
 import mindustry.entities.Units;
 import mindustry.game.EventType;
 import mindustry.gen.*;
 import mindustry.mod.Mod;
+import mindustry.net.Administration.*;
 import mindustry.ui.Styles;
 import mindustry.world.blocks.payloads.Payload;
 import mindustry.world.meta.BuildVisibility;
@@ -32,29 +33,75 @@ public class PUCMain extends Mod {
             globalPackets();
             buildButton();
 
-            netClient.addPacketHandler("wuc-true", s -> validHost = true);
+            netClient.addPacketHandler("puc-true", s -> validHost = true);
+            netClient.addPacketHandler("puc-enter", s -> takeHandler(s));
+            ui.settings.addCategory("@puc-settings", Icon.download, table -> {
+                table.checkPref("puc-enter-button", Version.number <= 8);
+                table.checkPref("puc-block-button", true);
+            });
+
         });
 
         Events.on(EventType.WorldLoadEvent.class, a ->{
             validHost = false;
             carryBlocks = false;
-            if(net.client()) Call.serverPacketReliable("wuc-check", "");
+            if(net.client()) Call.serverPacketReliable("puc-check", "");
             if(!headless) Time.run(0.5f * Time.toSeconds, this::rebuildSubButtons);
         });
 
     }
 
     public void globalPackets(){
-        netServer.addPacketHandler("wuc-check", (p, s) -> Call.clientPacketReliable(p.con(), "wuc-true", ""));
-        netServer.addPacketHandler("wuc-take", (p, s) -> {
-            String[] params = s.split(" ");
-            String[] ids = params[2].split("-");
-            Seq<Unit> list = new Seq<>();
-            for (String id : ids) {
-                list.add(Groups.unit.getByID(Integer.parseInt(id)));
+        Config limit = new Config("puc-RateLimit", "whether or now payload commanding is limited by Block interaction rate limit", true);
+        netServer.addPacketHandler("puc-check", (p, s) -> Call.clientPacketReliable(p.con(), "puc-true", ""));
+        netServer.addPacketHandler("puc-take", (p, s) -> {
+            if(!limit.bool()){
+                takePacket(s, p);
+                return;
             }
-            payloadHandler(params[0].equals("true"), params[1].equals("true"), list);
+
+            Ratekeeper rate = p.getInfo().rate;
+            if(rate.allow(Config.interactRateWindow.num() * 1000L, Config.interactRateLimit.num())) takePacket(s, p);
+            else{
+                if(rate.occurences > Config.interactRateKick.num()) p.kick("You are interacting with payload commands too quickly. (Puc", 1000 * 30);
+                else if(p.getInfo().messageTimer.get(60f * 2f)) p.sendMessage("[scarlet]You are interacting with payload commands too quickly.");
+            }
+
         });
+
+        netServer.addPacketHandler("puc-enter-req", (p, s) -> {
+            if(!limit.bool()){
+                sendTakeActual(s, p);
+                return;
+            }
+
+            Ratekeeper rate = p.getInfo().rate;
+            if(rate.allow(Config.interactRateWindow.num() * 1000L, Config.interactRateLimit.num())) sendTakeActual(s, p);
+            else{
+                if(rate.occurences > Config.interactRateKick.num()) p.kick("You are interacting with payload commands too quickly. (Puc", 1000 * 30);
+                else if(p.getInfo().messageTimer.get(60f * 2f)) p.sendMessage("[scarlet]You are interacting with payload commands too quickly.");
+            }
+
+        });
+    }
+
+    public void takePacket(String s, @Nullable  Player p){
+        if(p != null){
+            try{
+                netServer.writeEntitySnapshot(p);
+                netServer.writeBlockSnapshots();
+            } catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+
+        String[] params = s.split(" ");
+        String[] ids = params[2].split("-");
+        Seq<Unit> list = new Seq<>();
+        for (String id : ids) {
+            list.add(Groups.unit.getByID(Integer.parseInt(id)));
+        }
+        payloadHandler(params[0].equals("true"), params[1].equals("true"), list);
     }
 
     public void buildButton (){
@@ -76,7 +123,9 @@ public class PUCMain extends Mod {
         float pad = 1f;
         pucTable.table(Tex.pane, t ->{
             t.margin(0f);
-            t.button(Icon.distribution, Styles.clearNoneTogglei, () -> carryBlocks = !carryBlocks).update(l -> l.setChecked(carryBlocks)).name("puc-take-block").pad(pad).size(48f * scl).row();
+            //V7 back port
+            if(Core.settings.getBool("puc-enter-button"))t.button(Icon.download, Styles.clearNonei, this::enterButton).name("puc-enter-button").tooltip(Core.bundle.get("PUC.entertooltip")).wrap().pad(pad).size(42f * scl).row();
+            if(Core.settings.getBool("puc-block-button"))t.button(Icon.distribution, Styles.clearNoneTogglei, () -> carryBlocks = !carryBlocks).update(l -> l.setChecked(carryBlocks)).name("puc-take-block").pad(pad).size(48f * scl).row();
             t.button(Icon.up, Styles.clearNonei, () -> buttonHandler(true)).name("puc-take-button").pad(pad).size(42f * scl).row();
             t.button(Icon.down, Styles.clearNonei, () -> buttonHandler(false)).name("puc-drop-button").pad(pad).size(48f * scl).row();
         });
@@ -84,19 +133,18 @@ public class PUCMain extends Mod {
     }
 
     public void buttonHandler(boolean take) {
+        if(net.client() && !validHost) return;
         Seq<Unit> list = control.input.selectedUnits.copy();
-        if(list.size < 1) return;
         list.removeAll(u -> !(u instanceof Payloadc));
+        if(list.size < 1) return;
 
-        if (Vars.net.server() || !Vars.net.active()) {
-            payloadHandler(take, carryBlocks, list);
-        } if(net.client() && validHost){
+        if (Vars.net.server() || !Vars.net.active()) payloadHandler(take, carryBlocks, list);
+        if(net.client() || net.server()){
             StringBuilder ids = new StringBuilder();
             for(int i = 0; i < list.size; i++) ids.append(list.get(i).id).append("-");
-            Call.serverPacketReliable("wuc-take", Strings.format("@ @ @", take, carryBlocks, ids.toString()));
+            Call.serverPacketReliable("puc-take", Strings.format("@ @ @", take, carryBlocks, ids.toString()));
         }
     }
-
 
     public void payloadHandler(boolean take, boolean blocks, Seq<Unit> seq) {
         if (seq.size == 0) return;
@@ -130,6 +178,59 @@ public class PUCMain extends Mod {
     }
 
 
+    public void  enterButton(){
+        if(net.client() && !validHost)return;
+        Seq<Unit> list = control.input.selectedUnits.copy();
+        list.removeAll(u -> (u.buildOn() == null || !u.buildOn().canControlSelect(u)));
+        if(list.size < 1) return;
+
+
+        if(net.client()){
+            sendTakeRequest(list);
+
+        } else if(!net.active() || Vars.net.server()) {
+            if(Vars.net.server()){
+                StringBuilder ids = new StringBuilder();
+                for(int i = 0; i < list.size; i++) ids.append(list.get(i).id).append("-");
+                sendTakeActual(ids.toString(), null);
+            }
+            payloadEnter(list);
+        }
+    }
+
+    public void sendTakeRequest(Seq<Unit> list){
+        StringBuilder ids = new StringBuilder();
+        for(int i = 0; i < list.size; i++) ids.append(list.get(i).id).append("-");
+        Call.serverPacketReliable("puc-enter-req", ids.toString());
+    }
+
+    public void sendTakeActual(String s, @Nullable Player p){
+        if (Vars.net.server() || !Vars.net.active()) takeHandler(s);
+
+        if (Vars.net.server() || Vars.net.client()) {
+            Call.clientPacketReliable("puc-enter", s);
+        }
+    }
+
+    public void takeHandler(String s){
+        String[] ids = s.split("-");
+        Seq<Unit> list = new Seq<>();
+        for (String id : ids) {
+            Unit unt = Groups.unit.getByID(Integer.parseInt(id));
+            if(unt != null)list.add(unt);
+        }
+        payloadEnter(list);
+    }
+
+
+    public void payloadEnter(Seq<Unit> seq){
+        for (Unit unit : seq) {
+            Building b = unit.buildOn();
+            if(b != null && b.canControlSelect(unit)){
+                b.onControlSelect(unit);
+            }
+        }
+    }
 
 }
 
